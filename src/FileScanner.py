0c57bd7dd42a6c279aca8bd294780036530b3d54
Benjamin-Fever <ben.fever07@gmail.com>
2023-03-05 01:35:13 +1300

    add comments

--- src/FileScanner.py
+++ src/FileScanner.py
@@ -1,6 +1,9 @@
-# This is a helper class made by Benjamin Fever
+### This is a utility class made by Benjamin Fever
+### It follows the same idea as the java.util.Scanner
+### Makes it easier to scan through a large file
 
-class Scanner(object):
+class Scanner:
+    #  Sets the initial variables
     def __init__(self, filelocation):
         self.position = 0
         file = open(filelocation)
@@ -9,12 +12,13 @@ class Scanner(object):
         self.read_file = file_read.split()
         file.close()
 
-
+    #  Returns the next word as a string
     def next(self):
         value = self.read_file[self.position]
         self.position+=1
         return value
     
+    #  Returns the next int and sets the position to that point
     def next_int(self):
         value = None
         for i in range(self.position, len(self.read_file)):
@@ -27,6 +31,7 @@ class Scanner(object):
                 continue
         return value
     
+    #  Returns the next float and sets the position to that point
     def next_float(self):
         value = None
         for i in range(self.position, len(self.read_file)):
@@ -39,18 +44,18 @@ class Scanner(object):
                 continue
         return value
     
+    #  Reset position
     def reset(self):
         self.position = 0
     
+    #  Get size of scanner
     def size(self):
         return len(self.read_file)
     
+    #  Get line count
     def get_line_count(self):
         return self.line_count
     
+    #  Return true if there is a word next, else return false
     def has_next(self):
         return self.position < self.size()
-    
-    def skip(self, x):
-        for i in range(0, x):
-            self.next()
--- src/knn.py
+++ src/knn.py
@@ -1,24 +1,28 @@
-# K-Nearest-Neighbor by Benjamin Fever
+### K-Nearest-Neighbor by Benjamin Fever
+### This is a implementation of the K Nearest Neighbor Machine learning algorithm
+# TODO: Clean up code, read files from command line, check all testing data
 
-from FileScanner import Scanner
-from math import sqrt, pow
+# Import any needed librarys 
+from FileScanner import Scanner  # Custom library I made to help read files
+from math import sqrt
 from collections import OrderedDict
 
-filepath = __file__[0:len(__file__) - len("src\\knn.py")]
-
+# Function that returns the result of the min max scaling calculation
+# Let min(x) be the minimum value for x, Let max(x) be the maximum value for x. x - min(x) / max(x) - min(x) 
 def min_max_scaling(x, min, max):
     return ((x - min) / (max - min))
 
+# Calculate the euclidean distance between point1 and point2 sqrt((p1 - p2)^2)
 def euclidean_dist(point1, point2):
     total = 0
     for i in range(0, 13):
         total += (point1[i] - point2[i])**2
     return sqrt(total)
 
-def read_data(filename):
-    scanner = Scanner(filepath + filename)
+# Read the raw data files and return it as a nested list
+def read_data(filepath):
+    scanner = Scanner(filepath)
     data = []
-    scanner.skip(14)
     while scanner.has_next():
         value = []
         for i in range(0, 14):
@@ -26,8 +30,8 @@ def read_data(filename):
         data.append(value)
     return data
 
+#  Use the training data to find the mins and maxs for each category to use later
 def train_data(data):
-    # Train for the mins and maxs and normalizes the the training data
     mins = []
     maxs = []
     for x in range(0, 13):
@@ -42,17 +46,19 @@ def train_data(data):
         mins.append(min)
         maxs.append(max)
 
+    #  Normalize the data between values 0-1 so our data isn't skewed
     for point in data:
-        normalize_data(point, mins, maxs)
+        normalize_point(point, mins, maxs)
         
     return data, mins, maxs
 
-def normalize_data(point, mins, maxs):
+# This normalize a data point and all its values, returns the normalized point
+def normalize_point(point, mins, maxs):
     for i in range(0, 13):
         point[i] = min_max_scaling(point[i], mins[i], maxs[i])
     return point
 
-
+# This function calculates the nearest neigbors of count K and returns how many "votes" each class has
 def find_class(point1, data, k):
     # Get the distance for all points
     order = {}
@@ -71,12 +77,14 @@ def find_class(point1, data, k):
 
     return count
     
-
+# Train data
 data, mins, maxs = train_data(read_data("data\\wine-training"))
-k = 21
-point1 = normalize_data([12.93, 3.8, 2.65, 18.6, 102.0, 2.41, 2.41, 0.25, 1.98, 4.5, 1.03, 3.52, 770.0], mins, maxs)
+k = 21 # random k value
+point1 = normalize_point([1.79, 2.13, 2.78, 28.5, 92.0, 2.13, 2.24, 0.58, 1.76, 3.0, 0.97, 2.44, 466.0], mins, maxs) # selected point from data file
 
 class_counts = find_class(point1, data, k)
+
+# print what class the point is
 if (class_counts[0] > class_counts[1] and class_counts[0] > class_counts[2]):
     print("Class: 1")
 if (class_counts[1] > class_counts[0] and class_counts[1] > class_counts[2]):
